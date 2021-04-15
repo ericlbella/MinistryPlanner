24046097bd24ee654599ec7a21897bd9cd3108b7
Eric Louis Bella <ericlbella@gmail.com>
2021-04-15 12:43:26 -0400

    Pastor Controller and View added

---
+++ MinistryPlanner.Models/PastorListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinistryPlanner.Models
+{
+    public class PastorListItem
+    {
+        public string FirsName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public bool SeniorPastor { get; set; }
+        public bool AssistantPastor { get; set; }
+        public bool YouthPastor { get; set; }
+        public bool SongLeader { get; set; }
+    }
+}
