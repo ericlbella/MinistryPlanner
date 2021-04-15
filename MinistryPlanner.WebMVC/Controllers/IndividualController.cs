80bf2021a47c9d930c44477917c200abb6cb8694
Eric Louis Bella <ericlbella@gmail.com>
2021-04-14 22:08:04 -0400

    Individual Model Created

---
+++ MinistryPlanner.WebMVC/Controllers/IndividualController.cs
@@ -0,0 +1,20 @@
+﻿using MinistryPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinistryPlanner.WebMVC.Controllers
+{
+    [Authorize]
+    public class IndividualController : Controller
+    {
+        // GET: Individual
+        public ActionResult Index()
+        {
+            var model = new IndividualListItem[0];
+            return View(model);
+        }
+    }
+}
